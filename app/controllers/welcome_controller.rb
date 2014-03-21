886c41f2ce20dc7f7a687164b50ce8f6c879ffb1
Ben <ben9910@gmail.com>
2014-03-21 19:11:20 +0000

    Finished country/program nav menu

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,6 @@
 class WelcomeController < ApplicationController
   def index
   	@countries = Country.all.order( 'countries.name ASC' )
-  	#@programs = Program.all.order( 'programs.name ASC' )
   	@years = Year.all.order( 'year.year ASC' )
   end
 end
