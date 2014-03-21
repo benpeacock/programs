190481145f6e4f303b868d24e61a938f34a03ba7
Ben <ben9910@gmail.com>
2014-03-21 20:28:09 +0000

    Working on years index partial

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
   	@countries = Country.all.order( 'countries.name ASC' )
-  	@years = Year.all.order( 'year.year ASC' )
+  	@years = Year.all
   end
 end
