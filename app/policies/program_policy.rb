982fab968a4770ad11caa8b2025b32e1b352140e
Ben <benjaminwpeacock@gmail.com>
2014-03-24 20:19:12 +0000

    Adding class-specifc Pundit policies

---
+++ app/policies/program_policy.rb
@@ -0,0 +1,7 @@
+class ProgramPolicy < ApplicationPolicy
+  class Scope < Struct.new(:user, :scope)
+    def resolve
+      scope
+    end
+  end
+end
---
+++ app/policies/type_policy.rb
@@ -0,0 +1,7 @@
+class TypePolicy < ApplicationPolicy
+  class Scope < Struct.new(:user, :scope)
+    def resolve
+      scope
+    end
+  end
+end
