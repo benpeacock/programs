6c5033c70322ff1d19e65c199c25eef894e0386d
Ben <benjaminwpeacock@gmail.com>
2014-03-26 21:56:19 +0000

    Fixing year#show view

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,6 @@
 module QuestionsHelper
+	def answer_for_question(question, program, year)
+  	a = Answer.find_by program: @program, question: question, year: @year 
+  	a.nil? ? "" : a.answer
+	end
 end
