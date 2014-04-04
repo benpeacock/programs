7119a461e225a8a78a372d28506beb7cb74e19ad
Ben <benjaminwpeacock@gmail.com>
2014-04-04 15:47:31 +0000

    Copy Previous button works

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,2 +1,6 @@
 module AnswersHelper
+	def answer_for_question(question, program, year)
+  		a = Answer.find_by program: @program, question: question, year: @year 
+  		a.nil? ? "" : a
+  	end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,6 +1,3 @@
 module QuestionsHelper
-	def answer_for_question(question, program, year)
-  	a = Answer.find_by program: @program, question: question, year: @year 
-  	a.nil? ? "" : a
-	end
+
 end
