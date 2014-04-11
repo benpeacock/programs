eb7a1e8d4961c39d7603d62da4b04c991ebb4571
Ben <benjaminwpeacock@gmail.com>
2014-04-11 20:58:29 +0000

    minor changes

--- app/policies/answer_policy.rb
+++ app/policies/answer_policy.rb
@@ -2,16 +2,17 @@ class AnswerPolicy < ApplicationPolicy
   class Scope < Struct.new(:user, :scope)
 	# Changing default permission group of 'admin' to 'user' for these methods so that
 	# normal users can create and edit answers to questions.
-	# def create?
- #    user.present? && user.role?(:user)
- #  end
+		def create?
+	    user.present? && user.role?(:user)
+	  end
 
- #    def update?
- #    user.present? && user.role?(:user)
- #  end
-	 def resolve
-	 	scope
-	 end
+	  def update?
+	    user.present? && user.role?(:user)
+	  end
+
+	  def resolve
+	 	  scope
+	  end
+
    end
-
 end
